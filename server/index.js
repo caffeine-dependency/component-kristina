ab7afb44c26ceaf422e104e8c9b82e298f8dd582
Kristina Taing <kataing12@gmail.com>
2019-07-25 14:26:36 -0700

    [feat] added additional api calls to test seeded data

--- server/index.js
+++ server/index.js
@@ -2,15 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const path = require('path');
-
 const db = require('../database/index');
 
 const app = express();
-
 const port = 2002;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
@@ -18,14 +16,34 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 
 app.get('/api/viewer/products', (req, res) => {
   let randomIndex = Math.floor(Math.random() * 100) + 1;
-  db.findOne({index: randomIndex})
-    .then( (item) => {
-      console.log('Succcessful get!');
+  db.findOne({ index: randomIndex })
+    .then((item) => {
       res.status(200).send(item);
     })
-    .catch( (err) => {
-      console.log('Error getting', err);
+    .catch((err) => {
       res.status(404).send('Error getting item :(');
     }
     );
+})
+
+// For backend testing purposes
+app.get('/api/all', (req, res) => {
+  db.find({})
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      res.status(404).send('Could not get all items')
+    })
+})
+
+app.get('/api/:id', (req, res) => {
+  var { id } = req.params
+  db.findOne({index:id})
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => {
+      res.status(404).send('Could not get all items')
+    })
 })
