bce99fd0b0bb3f039946b9d1f9efd88637ceab26
Kristina Taing <kataing12@gmail.com>
2019-07-30 17:16:03 -0700

    [prog] changed the schema to match the csv data

--- database-postgresql/model.js
+++ database-postgresql/model.js
@@ -12,6 +12,7 @@ const Product = db.define('product', {
 })
 
 const Image = db.define('image', {
+  product_id: Sequelize.INTEGER,
   colors: Sequelize.ARRAY(Sequelize.STRING),
   urls: Sequelize.ARRAY(Sequelize.STRING),
   thumbnails: Sequelize.ARRAY(Sequelize.STRING)
@@ -20,6 +21,7 @@ const Image = db.define('image', {
 })
 
 const Size = db.define('size', {
+  product_id: Sequelize.INTEGER,
   size: Sequelize.STRING,
   in_stock: Sequelize.INTEGER,
 }, {
@@ -34,11 +36,11 @@ Product
 Image
   .sync({ force: false })
   .then(() => console.log('Image table has been created.'))
-  .catch((err) => console.error('Products table could not be created.', err));
+  .catch((err) => console.error('Image table could not be created.', err));
 
 Size
   .sync({ force: false })
   .then(() => console.log('Size table has been created.'))
-  .catch((err) => console.error('Products table could not be created.', err));
+  .catch((err) => console.error('Size table could not be created.', err));
 
 module.exports = { Product, Image, Size }
