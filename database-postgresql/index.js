8632f0f1af46672603b7506d57f160689e05ce74
Kristina Taing <kataing12@gmail.com>
2019-08-07 16:13:40 -0700

    [prog] changed db name

--- database-postgresql/index.js
+++ database-postgresql/index.js
@@ -1,7 +1,7 @@
 const { username, password, host } = require('./postgresql-token');
 
 const Sequelize = require('sequelize');
-const db = new Sequelize('test', username , password, {
+const db = new Sequelize('product', username , password, {
   host: host,
   dialect: 'postgres'
 });
@@ -9,6 +9,9 @@ const db = new Sequelize('test', username , password, {
 db
   .authenticate()
   .then(() => console.log('Connected to postgreSQL'))
-  .catch(() => console.error('Sequelize was unable to connect to the database.'));
+  .catch((err) => {
+    console.log(err);
+    console.error('Sequelize was unable to connect to the database.')
+  });
 
   module.exports = db;
