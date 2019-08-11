b191805af83a54be0b43a512145b6be96c06ef67
Kristina Taing <kataing12@gmail.com>
2019-08-11 13:51:29 -0700

    [prog] uncommented production code, so client axios request hits s3 instead of sample data

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -13,59 +13,59 @@ class App extends React.Component {
 //    >> This is to prevent too many calls to my AWS S3 image storage.
 
 //------------------------------------------------------------------
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     dataLoaded: false,
-  //     name: '',
-  //     description: '',
-  //     rating: 0,
-  //     reviews: 0,
-  //     price: 0,
-  //     images: {},
-  //     sizes: [],
-  //     currentImage: {},
-  //     currentSize: null,
-  //     currentColor: null 
-  //   }
-  // }
-
-  // // Fetches data from API/database, and triggers re-render once this is complete.
-  // componentDidMount() {
-  //   axios
-  //   .get('/api/viewer/products')
-  //   .then( ({data}) => {
-  //     let {name, description, rating, reviews, price, images, sizes } = data;
-  //     this.setState({
-  //       dataLoaded: true,
-  //       name,
-  //       description,
-  //       rating,
-  //       reviews,
-  //       price,
-  //       images,
-  //       sizes,
-  //       currentImage: { url: images.thumbnails[0], key: 0 },
-  //       currentColor: images.colors[0]
-  //     }, console.log('Successfully fetched item data'))
-  //   })
-  //   .catch( err => {
-  //     console.log('There was an error getting the item data :(', err);
-  //   })
-  // }
-
-  // When ready to present, COMMENT OUT THIS CODE:
   constructor(props) {
-    let { name, description, rating, reviews, price, images, sizes } = sampleData;
-    super(props); 
+    super(props);
     this.state = {
-      dataLoaded: true,
-      name, description, rating, reviews, price, images, sizes,
-      currentImage: { url: images.thumbnails[0], key: 0 },
+      dataLoaded: false,
+      name: '',
+      description: '',
+      rating: 0,
+      reviews: 0,
+      price: 0,
+      images: {},
+      sizes: [],
+      currentImage: {},
       currentSize: null,
-      currentColor: images.colors[0]
+      currentColor: null 
     }
   }
+
+  // Fetches data from API/database, and triggers re-render once this is complete.
+  componentDidMount() {
+    axios
+    .get('/api/viewer/products')
+    .then( ({data}) => {
+      let {name, description, rating, reviews, price, images, sizes } = data;
+      this.setState({
+        dataLoaded: true,
+        name,
+        description,
+        rating,
+        reviews,
+        price,
+        images,
+        sizes,
+        currentImage: { url: images.thumbnails[0], key: 0 },
+        currentColor: images.colors[0]
+      }, console.log('Successfully fetched item data'))
+    })
+    .catch( err => {
+      console.log('There was an error getting the item data :(', err);
+    })
+  }
+
+  // When ready to present, COMMENT OUT THIS CODE:
+  // constructor(props) {
+  //   let { name, description, rating, reviews, price, images, sizes } = sampleData;
+  //   super(props); 
+  //   this.state = {
+  //     dataLoaded: true,
+  //     name, description, rating, reviews, price, images, sizes,
+  //     currentImage: { url: images.thumbnails[0], key: 0 },
+  //     currentSize: null,
+  //     currentColor: images.colors[0]
+  //   }
+  // }
   
   // Updates the image showing in the ProductViewer, triggered by user click on color thumbnails
   changeCurrentImage(url, key) {
